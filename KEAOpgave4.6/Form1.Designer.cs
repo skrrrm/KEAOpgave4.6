733c0dccbcaa632a14b1d32a19987b27ed06100c
skrrrm <a@macbook-windows>
2020-03-30 12:10:22 +0200

    Add project files.

---
+++ KEAOpgave4.6/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace KEAOpgave4._6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonCalc = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.labelInfo2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonCalc
+            // 
+            this.buttonCalc.Location = new System.Drawing.Point(497, 191);
+            this.buttonCalc.Name = "buttonCalc";
+            this.buttonCalc.Size = new System.Drawing.Size(136, 42);
+            this.buttonCalc.TabIndex = 0;
+            this.buttonCalc.Text = "Calculate";
+            this.buttonCalc.UseVisualStyleBackColor = true;
+            this.buttonCalc.Click += new System.EventHandler(this.buttonCalc_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(70, 163);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(190, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Type max number:";
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Location = new System.Drawing.Point(70, 269);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(0, 25);
+            this.labelResult.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(75, 191);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(185, 31);
+            this.textBox1.TabIndex = 3;
+            // 
+            // labelInfo2
+            // 
+            this.labelInfo2.AutoSize = true;
+            this.labelInfo2.Location = new System.Drawing.Point(377, 163);
+            this.labelInfo2.Name = "labelInfo2";
+            this.labelInfo2.Size = new System.Drawing.Size(365, 25);
+            this.labelInfo2.TabIndex = 4;
+            this.labelInfo2.Text = "Calculate sum of all numbers to max:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(798, 446);
+            this.Controls.Add(this.labelInfo2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonCalc);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonCalc;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelResult;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label labelInfo2;
+    }
+}
+
---
+++ KEAOpgave4.6/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KEAOpgave4._6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCalc_Click(object sender, EventArgs e)
+        {
+            // Clear result label
+            labelResult.Text = "";
+
+            // Variables
+            int max = 0;
+            int i = 0;
+            int sum = 0;
+
+            // try-catch to get number from textbox
+            try
+            {
+                max = Convert.ToInt32(textBox1.Text); // converts to int
+            }
+            catch
+            {
+                MessageBox.Show("Numbers only!", "Error", MessageBoxButtons.OK); // Messagebox at error
+            }
+
+            /*
+             * I decided to include the last number
+             * ie the max number with (i <= max) in the calculation
+             * and not the number before max (i < max).
+            */
+            while (i <= max)
+            {
+                sum = i + sum;
+                i++;
+            }
+
+            // result text with sum of the while loop above
+            labelResult.Text = $"Sum of every number to and including max number: {sum}"; 
+        }
+    }
+}
